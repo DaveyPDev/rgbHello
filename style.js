987fb35a97ac0223bdd1176ef0b79690deef6b24
David <Davey.P.Dev@gmail.com>
2022-01-30 20:38:45 -0500

    upated html

---
+++ style.js
@@ -0,0 +1,13 @@
+function randomRGB() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rbg(${r}, ${g}, ${b})`;
+}
+
+const letters = document.querySelectorAll('.letter');
+const intervalId = setInterval(function () {
+    for ( let letter of letters ) {
+        letters.style.color = randomRGB();
+    }
+}, 1000);
